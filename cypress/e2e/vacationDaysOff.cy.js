6a730e77f5999375bce6f438bf1d8f273567a506
k0ta99 <kotoganmarko5@gmail.com>
2023-02-24 16:19:36 +0100

    added vacationDaysOff tests

---
+++ cypress/e2e/vacationDaysOff.cy.js
@@ -0,0 +1,62 @@
+/// <reference types="Cypress"/>
+
+import { orgConfig } from "../pageObjects/orgConfiguration";
+import { faker } from "@faker-js/faker";
+import {flatpickr} from "flatpickr";
+
+
+describe("check if reported days off deduct from total number of days off  ", () =>{
+   
+    let organizationId;
+    let boardId;
+
+    before("set up state", () => {
+        cy.loginViaBackend();
+        cy.createOrg();
+        cy.readFile('cypress/fixtures/ids.json').then((ids) =>{
+            cy.wrap(ids).as('orgId');
+            organizationId = ids.organizationId;
+        })
+        cy.addBoard();
+        cy.readFile('cypress/fixtures/ids.json').then((ids) =>{
+            cy.wrap(ids).as('boardId');
+            boardId = ids.boardId;
+        })
+        cy.addUser();
+       
+    })
+
+    beforeEach("visit organization", () =>{
+            cy.loginViaBackend();
+            cy.visit(`/organizations/${organizationId}/boards`);
+            cy.url().should("include", organizationId);
+    })
+
+    it.only("set vacation days", () =>{
+        orgConfig.boardsPop.click()
+        orgConfig.configPanel.should("be.visible")
+        .and("have.css", "background-color", 'rgb(204, 204, 204)')
+
+        orgConfig.setVacationDays();
+        orgConfig.memberModal.should("be.visible")
+        .and("have.css", "background-color", 'rgb(255, 255, 255)')
+        cy.get('.is-left > .el-date-table > tbody > :nth-child(5) > :nth-child(7)').click()
+        cy.get('.is-left > .el-date-table > tbody > :nth-child(6) > :nth-child(3)').click()
+        orgConfig.addButton.click()
+        orgConfig.vacationDays.should("have.value", 14)
+
+    })
+
+    it("check if vacation days changed", () =>{
+        cy.visit(`/boards/${boardId}/team`)
+        cy.url().should("include", boardId)
+        orgConfig.checkIfVacationDaysChanged();
+        orgConfig.timeOffTab.should("be.visible")
+        .and("have.text", "Time Off")
+        orgConfig.numberOfVacationDaysOnThisDay.should("have.value", orgConfigNumbers.vacationDays - 5);
+    })
+
+    after("delete org", () =>{
+        cy.deleteOrg();
+    })
+})
--- cypress/pageObjects/orgConfiguration.js
+++ cypress/pageObjects/orgConfiguration.js
@@ -28,15 +28,51 @@ class OrgConfig{
     }
 
     get enterTeamMember(){
-        return cy.get(".vs-u-cursor--pointer").eq(0)
+        return cy.get(".vs-u-cursor--pointer").find(".vs-u-img--round")
     }
 
     get timeOffTab(){
-        return cy.get(".el-tabs__nav-scroll").find(".el-tabs__item").eq(2)
+        return cy.get(".el-tabs__nav-scroll").find(".el-tabs__item").eq(5)
     }
 
     get numberOfVacationDaysOnThisDay(){
         return cy.get(".vs-c-free-days").find("input[type='number']")
+    }
+
+    get teamManagement(){
+        return cy.get(".vs-l-project__menu").find(".vs-c-site-logo").eq(5)
+    }
+
+    get addEvent(){
+        return cy.get('[style=""] > :nth-child(1) > .vs-c-timeline > .vs-c-timeline__add-activity > .vs-c-timeline__activity > .vs-c-media')
+    }
+
+    get calender(){
+        return cy.get(':nth-child(1) > .el-date-editor > .el-input__icon')
+    }
+
+    get startDate(){
+        return cy.get(".available in-range start-date");
+    }
+
+    get vacationDaysHeader(){
+        return cy.get(".vs-u-gap--bottom").find(".vs-u-color--primary vs-u-gap--bottom-xs vs-u-text--uppercase")
+    }
+
+    get contactEmail(){
+        return cy.get(".vs-c-form-group__body").find("input[type='email']")
+    }
+
+    get addButton(){
+        return cy.get('.vs-u-display--flex > :nth-child(2) > :nth-child(2)')
+    }
+
+    get vacationDays(){
+        return cy.get(".vs-c-free-days-form-group").eq(1).find("input[type='number']")
+    }
+
+    get memberModal(){
+        return cy.get(".vs-c-modal__body")
     }
 
     changeVacationDaysPerYear(daysNumber){
@@ -49,7 +85,16 @@ class OrgConfig{
     checkIfVacationDaysChanged(){
         cy.get(".vs-c-team-member__avatar").first().click() 
         this.timeOffTab.click();
-        
+    }
+
+    setVacationDays(){
+        this.teamManagement.click()
+        this.boardsPop.click()
+        this.enterTeamMember.click();
+        this.timeOffTab.click();
+        this.addEvent.click({force: true});
+        this.calender.click();
+       
 
     }
 }
